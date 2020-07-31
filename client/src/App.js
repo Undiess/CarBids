238748f966dd14b7876c1a66fbafcdfbcc54200a
Andy Nguyen <nguyen.andy67@gmail.com>
2020-07-31 15:07:54 +0930

    Added react apllication

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar/Navbar.js'
+
+
+function App() {
+  return (
+    <div> 
+      <Navbar/> 
+    </div> 
+
+  );
+}
+
+export default App;
