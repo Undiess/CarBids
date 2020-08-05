2f8fc586f90eb05c3f348772536fb6247689dfda
Undiess <nguyen.andy67@gmail.com>
2020-08-05 20:12:33 +0930

    Login and signup page working

--- client/src/components/pages/signIn/signup.js
+++ client/src/components/pages/signIn/signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "./stylesheet.css"
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import API from "../../utils/API"
 
 
@@ -17,9 +17,25 @@ function SignUp(){
         setState({...state,[e.target.id]: e.target.value });
       };
 
+    const [nameError,setNameError]=useState({
+        nameError:""
+    });
+
+    const [emailError,setEmailError]=useState({
+        emailError:""
+    });
+
+    const [passwordError,setPasswordError]=useState({
+        passwordError:""
+    });
       
   const onSubmit = e => {
     e.preventDefault();
+
+    setNameError({});
+    setEmailError({});
+    setPasswordError({})
+
     const userData = {
         name: state.name,
         email: state.email,
@@ -27,7 +43,25 @@ function SignUp(){
         password2:state.password2
     };
     API.postRegister(userData).then(res =>{
-        console.log(res)
+       
+     
+        if (res.data.name){
+            
+            setNameError({nameError:res.data.name})
+        };
+        if (res.data.email){
+            setEmailError({emailError:res.data.email})
+            
+        };
+        if (res.data.password){
+            setPasswordError({passwordError:res.data.password})
+        }
+
+        if (res.data.password2){
+            setPasswordError({passwordError:res.data.password2})
+        }
+        
+    
     })
     
      }
@@ -46,9 +80,12 @@ function SignUp(){
             <p> OR</p>
 
             <input onChange={onChange} type="text" id="name" class="form-control" placeholder="Full name" required="" autofocus=""/>
+            <p class="warninglabel"> {nameError.nameError}</p>
             <input onChange={onChange} type="email" id="email" class="form-control" placeholder="Email address" required autofocus=""/>
+            <p class="warninglabel"> {emailError.emailError}</p>
             <input onChange={onChange} type="password" id="password" class="form-control" placeholder="Password" required autofocus=""/>
             <input onChange={onChange} type="password" id="password2" class="form-control" placeholder="Repeat Password" required autofocus=""/>
+            <p class="warninglabel"> {passwordError.passwordError}</p>
 
             <button onClick={onSubmit} class="btn btn-primary btn-block" type="submit"><i class="fas fa-user-plus"></i> Sign Up</button>
             <Link to="/signin"><a href="#" id="cancel_signup"><i class="fas fa-angle-left"></i> Back</a> </Link>
